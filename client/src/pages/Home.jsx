5d0a7ed9eb80fb96aad6b6c9e4052e4b8771c0bd
anirbank-00 <maverick.mern02@gmail.com>
2025-01-27 14:21:43 +0530

    homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,9 @@ import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-c over bg-center'>
+    // <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-c over bg-center'>
+    <div  className='flex items-center justify-center min-h-screen bg-gradient-to-br
+    from-blue-300 to-purple-400  '>
       <Navbar/>
       <Header/>
     </div>
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
+authRouter.post("/register", register); 
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
